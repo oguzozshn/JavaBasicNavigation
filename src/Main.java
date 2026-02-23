1adca223984cc4192e4e8aaef7634b2899ca5838
Oguz Ozsahin <oguzozshn@gmail.com>
2026-02-24 01:42:00 +0300

    feat: implement CSV reader to filter admin and primary cities

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 void main() {
+    String fileName = "tr.csv";
+    File file = new File(fileName);
 
+    if (!file.exists()) {
+        System.out.printf("%s can not be found.", fileName);
+        System.exit(1);
+    }
+
+    try (Scanner inputFile = new Scanner(file)) {
+        System.out.printf("OK: %s opened.%n", fileName);
+
+        int capitalIndex = 6;
+        while (inputFile.hasNextLine()){
+            String line = inputFile.nextLine();
+            String[] values = line.split(",");
+
+
+            String capital = values[capitalIndex].trim();
+            if ("admin".equalsIgnoreCase(capital) || "primary".equalsIgnoreCase(capital)) {
+                System.out.println(values[0] + "," + values[1] + "," + values[2]);
+            }
+        }
+    } catch (FileNotFoundException e) {
+        System.out.printf("%s can not be opened.%n", fileName);
+    }
 }
